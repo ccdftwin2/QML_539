4cb81e4554fa5fe8b79ae06fde0688a267c04a59
Collin <ccdftwin2@gmail.com>
2022-11-19 19:21:57 -0600

    update

---
+++ Models/QKM/qsvm_heart.py
@@ -0,0 +1,78 @@
+import numpy as np
+import pandas as pd
+import os
+import time
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import pennylane as qml
+from pennylane.templates import AngleEmbedding, StronglyEntanglingLayers
+from pennylane.operation import Tensor
+import matplotlib.pyplot as plt
+
+# get the current directory
+cur_dir = os.getcwd()
+
+# Import the dataset
+df = pd.read_csv(os.path.join(cur_dir, '../../Datasets/normalized_heart.csv'), header=0)
+
+# split into x, y
+X, y = df.drop("output", axis=1), df["output"]
+
+# Need to scale the data to normal distribution for embeddings to work
+scaler = StandardScaler().fit(X)
+X_scaled = scaler.transform(X)
+print("Mean: ", X_scaled.mean(), "Standard Deviation: ", X_scaled.std())
+y_scaled = 2 * (y - 0.5)
+print("Unique labels:", np.unique(y_scaled))
+
+# Train/test split for model development
+X_train, X_test, y_train, y_test = train_test_split(X_scaled, y_scaled)
+
+# Embed the data. First define the number of qubits
+n_qubits = len(X_scaled[0])
+
+# Set up the projector
+dev_kernel = qml.device("default.qubit", wires=n_qubits)
+
+projector = np.zeros((2**n_qubits, 2**n_qubits))
+projector[0, 0] = 1
+
+# Define the kernel
+@qml.qnode(dev_kernel)
+def kernel(x1, x2):
+    """The quantum kernel."""
+    AngleEmbedding(x1, wires=range(n_qubits))
+    qml.adjoint(AngleEmbedding)(x2, wires=range(n_qubits))
+    return qml.expval(qml.Hermitian(projector, wires=range(n_qubits)))
+
+# Needed for SVC function
+def kernel_matrix(A, B):
+    return np.array([[kernel(a, b) for b in B] for a in A])
+
+# Start time
+start = time.time()
+
+# Define the svm
+svm = SVC(kernel=kernel_matrix, verbose=True).fit(X_train, y_train)
+
+#train time
+train_time = time.time() - start
+
+# Predict the test set
+prediction = svm.predict(X_test)
+
+# predict time
+predict_time = time.time() - train_time - start
+
+# Write the results to the test file
+f = open(os.path.join(cur_dir, '../../Results/qsvm_linear_heart.txt'), 'w')
+f.write(str(confusion_matrix(y_test,prediction)) + "\n")
+f.write(str(classification_report(y_test, prediction)))
+f.write("Accuracy:" + str(accuracy_score(y_test, prediction)*100) + "%\n\n")
+f.write("Training Time: " + str(train_time) + "\n")
+f.write("Prediction Time: " + str(predict_time) + "\n")
+f.close()
+
